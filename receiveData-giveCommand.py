29e0c131f48de56cefd55e2459d4be5c2c45e73f
NimaShamandi <143939602+ArchArtNimaAI@users.noreply.github.com>
2024-07-28 02:50:47 -1200

    Delete receiveData-giveCommand.py

--- receiveData-giveCommand.py
+++
@@ -1,8 +0,0 @@
-sia utente
-sia sistema automatico (chi lo controlla?)
-si manda comando (ma con MQTT o solo dire spegniti luce?)
-e poi dire lo stato della luce è questo cosi utenten e sistema automatico lo sanno (notify?)
-
-subscriber deve sempre avere notify e lo stop
-
-sia publisher sia subscriber 1 connect 2 start loop 3 publich/subscribe
